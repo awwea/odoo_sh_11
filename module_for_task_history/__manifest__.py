3bd2dc380fa036379ec1ebeea33ad3e55a564fcb
abdillahwahab <mdillahw@nurosoft-indonesia.com>
2017-12-09 15:41:18 +0700

    my module

---
+++ module_for_task_history/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Task History of Version",
+    "author": "abdillahwahab",
+    "website": "www.abdillahwahab.com",
+    "version": "1.0.0",
+    "category": "project",
+    "depends": [
+        "project"
+    ],
+    "data": [ 
+        "view/task.xml"
+    ],
+    "qweb": [
+    ],
+    'price': '50',
+    'currency': "USD",
+
+    "license": 'LGPL-3',
+    'installable': True
+}
